5812617b8fe50a6540eaca7dcc8d4468da9f9c93
dg0397 <rgg0397@gmail.com>
2020-10-29 13:13:48 -0400

    user stories 4-10

--- map.js
+++ map.js
@@ -3,8 +3,135 @@ async function drawMap(){
 
     //Fetching data
 
-    const dataset = await d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json")
+    const  dataset = await d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json")
 
-    console.log(dataset)
+    const colorData = {
+        'Wii' : '#f45d51',
+        'DS' : '#3a3e70',
+        'X360' : '#e2F4FF',
+        'GB' : '#97b5cc',
+        'PS3' : '#374667',
+        'NES' : '#63cace',
+        'PS2' : '#d54153',
+        '3DS' : '#ac8daf',
+        'PS4' : '#3282b8',
+        'SNES' : '#718ca1',
+        'PS' : '#3f5a92',
+        'N64' : '#2e6186',
+        'GBA' : '#942246',
+        'XB' : '#484c7f',
+        'PC' : '#bbe1fa',
+        '2600' : '#59748c',
+        'PSP' : '#5579c7',
+        'XOne' : '#274765'
+    }
+    //2) Create Chart Dimensions
+
+    let dimensions = {
+        width: window.innerWidth * 0.9 <= 600 ? window.innerWidth * 0.9 : 1200,
+        height: 500,
+    }
+
+
+    //3) Draw Canvas 
+
+    //adding main svg 
+
+    const canvas = d3.select("#canvas")
+                        .append('svg')
+                        .attr('width',dimensions.width)
+                        .attr('height',dimensions.height)
+
+    //adding bound(framework or whiteboard)
+
+    const bounds = canvas.append('g')
+                            .style('transform', `translate(${
+                               0
+                            }px, ${
+                                0
+                            }px)`);
+
+    //4) Create Scales 
+    
+    //setting Hierarchy
+    const hierarchy = d3.hierarchy(dataset,(node)=> node['children'])
+                        .sum((node)=> node['value'])
+                        .sort((node1,node2) => node2['value'] - node1['value'])
+                    
+
+    const createTreeMap = d3.treemap()
+                            .size([dimensions.width,dimensions.height])
+
+    //5) Draw Data
+    
+    //drawing data
+
+    createTreeMap(hierarchy)
+
+    const gamesTiles = hierarchy.leaves()
+
+    const block = bounds.selectAll('g')
+                        .data(gamesTiles)
+                        .enter()
+                        .append('g')
+                        .attr('transform', d => `translate( ${d['x0']}, ${d['y0']} )`)
+
+    const tile = block.append('rect')
+                        .attr('class','tile')
+                        .attr('fill',(d)=>{
+                            const {data : {category} } = d
+                            return colorData[category]
+                        })
+                        .attr('data-name',d => d['data']['name'])
+                        .attr('data-category',d => d['data']['category'])
+                        .attr('data-value',d => d['data']['value'])
+                        .attr('width', d => d['x1'] - d['x0'])
+                        .attr('height', d => d['y1'] - d['y0'])
+
+    block.append('text')
+        .text(d => d['data']['name'])
+        .attr('x',0)                
+        .attr('y',20)           
+        
+    //6)Draw Peripherals
+
+    //select legeng element
+    const legend = d3.select('#legend')
+                        .append('svg')
+                        .attr('width', dimensions.width * .4)
+                        .attr('height', dimensions.height * .4)
+    
+    const legendBlocks = legend.selectAll('g')
+                                .data(Object.values(colorData))
+                                .enter()
+                                .append('g')
+                                .attr('transform',(d,i) =>{
+                                    if( i < 3) return `translate(${(i - 0) * 150}, ${0})`
+                                    if( i < 6) return `translate(${(i - 3) * 150}, ${30})`
+                                    if( i < 9) return `translate(${(i - 6) * 150}, ${60})`
+                                    if( i < 12) return `translate(${(i - 9) * 150}, ${90})`
+                                    if( i < 15) return `translate(${(i - 12) * 150}, ${120})`
+                                    if( i < 18) return `translate(${(i - 15 )* 150}, ${150})`
+                                })
+                                .attr('class','legend-element')
+
+    const legendRect = legendBlocks.append('rect')
+                                    .attr('width',30)
+                                    .attr('height',30)
+                                    .attr('fill',d => d )
+                                    .attr('x',(d,i) =>  0)
+                                    .attr('y',(d,i) => 0 )
+                                    .attr('class','legend-item')
+
+    const textLegend = legendBlocks.append('text')
+                                    .text((d,i) => Object.keys(colorData)[i] )
+                                    .attr('x',45)
+                                    .attr('y',20)
+
+
+
+    //adding legend
+
+    console.log(gamesTiles)
 }
 drawMap()
